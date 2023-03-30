c197a226e3c87d164bddf5ee7134a0d92778f90e
jberger2992 <jberger2992@gmail.com>
2023-03-30 01:21:25 -0400

    alert/prompt/confirm code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePass1() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,4 +11,54 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePass1);
+
+// ahhhhhhhhhhhhhh
+
+let numOfChar = "";
+let specialChar = false;
+let numChar = false;
+let lowerChar = false;
+let upperChar = false;
+let passChars = [""];
+
+function writePass1(){
+  let passFirst = prompt("How many characters would you like your password to contain?");
+
+    if (passFirst < 8){
+      alert("The password must contain at least 8 characters.")
+    }
+    else if (passFirst > 128){
+      alert("The password can contain at most 128 characters.")
+    }
+    else{
+      numChar = passFirst;
+      alert(passFirst)
+      writePass2();
+    }
+}
+
+function writePass2(){
+  if (confirm("Click OK to confirm including special characters.")){
+    specialChar = true;
+  } else{specialChar = false}
+  if (confirm("Click OK to confirm including numbers.")){
+    numChar = true;
+  } else{numChar = false}
+  if (confirm("Click OK to confirm using lowercase letters.")){
+    lowerChar = true;
+  } else{lowerChar = false}
+  if (confirm("Click OK to confirm using uppercase letters.")){
+    upperChar = true;
+  } else{numChar = false}
+  if (specialChar || numChar || lowerChar || upperChar === true){
+    writePass33();
+  } else{
+    alert("Must confirm at least one option")
+    writePass2();
+  }
+}
+
+function writePass3(){
+
+}
