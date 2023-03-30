b392dce63ae058600f86359a0c21be81a3d7dd5d
jberger2992 <jberger2992@gmail.com>
2023-03-30 12:09:15 -0400

    js updates

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ let specialChar = false;
 let numChar = false;
 let lowerChar = false;
 let upperChar = false;
-let passChars = [""];
 
 function writePass1(){
   let passFirst = prompt("How many characters would you like your password to contain?");
@@ -32,13 +31,13 @@ function writePass1(){
       alert("The password can contain at most 128 characters.")
     }
     else{
-      numChar = passFirst;
-      alert(passFirst)
+      numOfChar = passFirst;
       writePass2();
     }
 }
 
 function writePass2(){
+  alert(numOfChar)
   if (confirm("Click OK to confirm including special characters.")){
     specialChar = true;
   } else{specialChar = false}
@@ -52,13 +51,31 @@ function writePass2(){
     upperChar = true;
   } else{numChar = false}
   if (specialChar || numChar || lowerChar || upperChar === true){
-    writePass33();
+    writePass3();
   } else{
     alert("Must confirm at least one option")
     writePass2();
   }
 }
 
-function writePass3(){
+let ranPass = []
 
+function writePass3(){
+  let passChars = [];
+  if(specialChar === true){
+    passChars.push("!","#","$","%","&","(","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","{","|","}","~");
+  }
+  if(numChar === true){
+    passChars.push("1","2","3","4","5","6","7","8","9","0");
+  }
+  if(lowerChar === true){
+    passChars.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+  }
+  if(upperChar === true){
+    passChars.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+  }
+  for (let i = 0; i < numOfChar; i++) {
+    drawRandom = passChars[Math.floor(Math.random() * numOfChar)];
+    ranPass.push(drawRandom);
+  }
 }
